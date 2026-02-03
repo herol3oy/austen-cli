7af2821132deffc8fc1e7031bd79966b22caf11c
Hamed Sedighi <hamedsedighi@gmail.com>
2026-02-03 14:45:34 +0100

    update logo

--- src/utils/logo.ts
+++ src/utils/logo.ts
@@ -1,13 +1,11 @@
-import figlet from 'figlet';
-import chalk from 'chalk';
+const AUSTEN_ASCII_LOGO = `
+ █████  ██    ██ ███████ ████████ ███████ ███    ██ 
+██   ██ ██    ██ ██         ██    ██      ████   ██ 
+███████ ██    ██ ███████    ██    █████   ██ ██  ██ 
+██   ██ ██    ██      ██    ██    ██      ██  ██ ██ 
+██   ██  ██████  ███████    ██    ███████ ██   ████
+`
 
-export function displayLogo(): void {
-  const logo = figlet.textSync('AUSTEN', {
-    font: 'Standard',
-    horizontalLayout: 'default',
-    verticalLayout: 'default',
-  });
-
-  console.log(chalk.cyan(logo));
-  console.log(chalk.gray('  Generate character relationship diagrams for any book\n'));
+export function displayLogo() {
+  console.log(AUSTEN_ASCII_LOGO)
 }
